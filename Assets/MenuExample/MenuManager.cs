6d14062803c9893eb34c0b89af9fd6f5d00f4b33
James Ledger <Jamesledger99@gmail.com>
2020-07-19 18:42:45 +0100

    Creating a template quest project to make handtracking development easier

---
+++ Assets/MenuExample/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject menuObject;
+    public GameObject handObject;
+    public GameObject headObject;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    menuObject.transform.position = handObject.transform.position + new Vector3(xOffset, yOffset, zOffset);
+
+      //  Vector3 direction_to_model = headObject.transform.position - menuObject.transform.position;
+        //Quaternion rotation = Quaternion.LookRotation(direction_to_model, Vector3.up);
+     //   menuObject.transform.rotation = rotation;
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Duck-hunt-VR
+A recreation of duck hunt in VR using finger guns to shoot
+
+Currently contains 2 modes: Moving target shooting and clay pigeon shooting. 
+
+Make a finger gun with index finger and thumb to load gun, then touch thumb to base of index finger to fire 
